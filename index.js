348df566fc564a5201ba804db3745fc859fb2faf
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2024-06-04 18:18:34 -0300

    Ajustando

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ app.use(express.json());
 const convidados = ['Alice', 'Bob', 'Charlie', 'David'];
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello Worlds');
 });
 
 app.get('/convidado/:nome', (req, res) => {
   const nome = req.params.nome;
   const estaNaLista = convidados.includes(nome);
   if (estaNaLista) {
-    res.send({ mensagem: 'Sim, você está na lista!', nome: nome });
+    res.send({ mensagem: 'Sim, você está na lista!' });
   } else {
     res.send({ mensagem: 'Não, você não está na lista!' });
   }
 });
 
-// Iniciar o servidor apenas se não estiver no ambiente de produção do Railway
-if (!process.env.PORT) {
-  app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
