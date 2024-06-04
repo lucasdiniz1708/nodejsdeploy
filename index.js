651010bb141ba17b3c144b0370e680d4ff434858
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2024-06-04 17:44:17 -0300

    Terceiro commit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const convidados = ['Alice', 'Bob', 'Charlie', 'David'];
 
 app.get('/', function (req, res) {
-  res.send('Hello Worlds')
-})
+  res.send('Hello Worlds');
+});
 
-app.listen(process.env.PORT || 3000)
+app.get('/convidado/:nome', (req, res) => {
+  const nome = req.params.nome;
+  const estaNaLista = convidados.includes(nome);
+  if (estaNaLista) {
+    res.send({ mensagem: 'Sim, você está na lista!' });
+  } else {
+    res.send({ mensagem: 'Não, você não está na lista!' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
